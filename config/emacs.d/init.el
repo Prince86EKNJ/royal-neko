9c1fee021444aa0bb81645c91e2d22a62f0a0de2
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-07-02 12:08:07 -0500

    Added line numbers and truncated lines

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -2,3 +2,19 @@
 	backup-directory-alist `(("." . ,temporary-file-directory))
 	auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
 )
+
+(add-hook 'text-mode-hook 'linum-mode)
+(add-hook 'prog-mode-hook 'linum-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(truncate-lines t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
