13b2cec3051776a06e171619047ab21f05f8f086
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-07-10 12:21:48 -0500

    More emacs settings

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -4,32 +4,57 @@
 	auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
 )
 
-;; Extra package repos
+;; Extra Package Repos
 (require 'package)
 (push '("melpa" . "http://stable.melpa.org/packages/") package-archives)
 (package-initialize)
 
 ;; Package list - figure out how to turn this into a command
-;; projectile
-;; magit
+(setq local-package-list '(projectile magit))
 
 ;; Functions
 (defun open-init-file ()
 	(interactive)
-	(setq bak vc-follow-symlinks)
-	(setq vc-follow-symlinks nil)
+	(setq 
+		defaultVal vc-follow-symlinks
+		vc-follow-symlinks nil)
 	(find-file user-init-file)
-	(setq vc-follow-symlinks bak)
+	(setq vc-follow-symlinks defaultVal)
 )
 
-;; Key bindings
-(global-set-key (kbd "C-c C-i") 'open-init-file)
+(defun open-line-forwards ()
+	(interactive)
+	(move-end-of-line nil)
+	(newline)
+)
+
+(defun open-line-backwards ()
+	(interactive)
+	(move-beginning-of-line nil)
+	(newline)
+	(previous-line)
+)
+
+(defun reload-init-file ()
+	(interactive)
+	(load-file user-init-file)
+)
+
+;; Key Bindings
 (global-set-key (kbd "C-p") 'projectile-find-file)
 (global-set-key (kbd "C-x g") 'magit-status)
 
+(global-set-key (kbd "C-c C-i") 'open-init-file)
+(global-set-key (kbd "C-c C-r") 'reload-init-file)
+
+(global-set-key (kbd "C-o") 'open-line-forwards)
+(global-set-key (kbd "C-S-o") 'open-line-backwards)
+(global-set-key (kbd "TAB") 'self-insert-command)
+
+;; Global Modes
 (projectile-global-mode)
 
-;; Mode hooks
+;; Mode Hooks
 (add-hook 'text-mode-hook 'linum-mode)
 (add-hook 'prog-mode-hook 'linum-mode)
 
@@ -38,6 +63,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backward-delete-char-untabify-method nil)
+ '(electric-indent-mode nil)
  '(linum-format "%d ")
  '(truncate-lines t))
 (custom-set-faces
