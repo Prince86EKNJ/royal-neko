8937bbe9f18e291c6b8f89210679b6018fcb64e2
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-07-10 23:57:05 -0500

    Updated magit faces

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -98,4 +98,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-diff-added ((t (:foreground "#335533"))))
+ '(magit-diff-added-highlight ((t (:foreground "#336633"))))
+ '(magit-diff-removed ((t (:foreground "#553333"))))
+ '(magit-diff-removed-highlight ((t (:foreground "#663333")))))
